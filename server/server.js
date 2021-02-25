5118a5df78e64d49fc8c5f110ef77cddbba7e101
Marwan <marwan.esmaail85@gmail.com>
2021-02-25 13:00:07 +0100

    Setting up npm start for back and front-end

--- server/server.js
+++ server/server.js
@@ -7,3 +7,13 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
