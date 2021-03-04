dc7184450c640fb4cb18cb80db38e47bf5121332
Marwan <marwan.esmaail85@gmail.com>
2021-03-04 09:56:20 +0100

    Adding Navbar to landing page

--- client/src/landingPage/landingPage.js
+++ client/src/landingPage/landingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Logo from "../landingPageComponents/logo";
+import PrimarySearchAppBar from "../landingPageComponents/navbar";
 
 function LandingPage() {
   return (
     <div>
       <Logo />
+      <PrimarySearchAppBar />
     </div>
   );
 }
