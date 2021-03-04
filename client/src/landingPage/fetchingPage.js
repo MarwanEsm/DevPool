3905655cf1ee8f61646b9be69897e369614f3765
Marwan <marwan.esmaail85@gmail.com>
2021-03-04 15:51:50 +0100

    Display Fetch

--- client/src/landingPage/fetchingPage.js
+++ client/src/landingPage/fetchingPage.js
@@ -1,28 +1,58 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import Nav from "react-bootstrap/Nav";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-// function Fetch() {
-//   const [jobs, setJobs] = useState([]);
+function Fetch() {
+  const [jobs, setJobs] = useState([]);
 
-//   useEffect(() => {
-//     fetch("http://localhost:5000/job/all")
-//       .then((res) => res.json())
-//       .then((data) => setJobs(data));
-//   }, []);
-//   return (
-//     <div>
-//       {jobs.length &&
-//         jobs.map((job) => {
-//           return (
-//             <div key={job._id}>
-//               <h1>{job.title}</h1>
-//               <h1>{job.location}</h1>
-//               <h2>{job.salary}</h2>
-//               <h2>{job.description}</h2>
-//             </div>
-//           );
-//         })}
-//     </div>
-//   );
-// }
+  useEffect(() => {
+    fetch("http://localhost:5000/job/all")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
+  return (
+    <div style={divStyle}>
+      {jobs.length &&
+        jobs.map((job) => {
+          return (
+            <div key={job._id}>
+              <Card>
+                <Card.Header>
+                  <Nav variant="pills" defaultActiveKey="#first">
+                    <Nav.Item>
+                      <Nav.Link href="#first">Add to watchlist</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="#link">Remove from watchlist</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title>Title: {job.title}</Card.Title>
+                  <Card.Text>Location:{job.location}</Card.Text>
+                  <Card.Text>Salary:{job.salary}</Card.Text>
+                  <Card.Text>Description:{job.description}</Card.Text>
+                  <Nav.Link href="#link">....Read more</Nav.Link>
+                  <Button variant="primary">Apply</Button>
+                </Card.Body>
+              </Card>
 
-// export default Fetch;
+              {/* <h3>Title:{job.title}</h3>
+              <h4>Location:{job.location}</h4>
+              <h4>Salary:{job.salary}</h4>
+              <h5>Description:{job.description}</h5> */}
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+const divStyle = {
+  width: "27%",
+  marginTop: "2%",
+  marginLeft: "2%",
+};
+
+export default Fetch;
--- client/src/landingPage/landingPage.js
+++ client/src/landingPage/landingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Headbar from "../landingPageComponents/navbar";
+import Fetch from "./fetchingPage";
 
 function LandingPage() {
   return (
     <div>
       <Headbar />
+      <Fetch />
     </div>
   );
 }
