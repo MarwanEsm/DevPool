b4a1ef0125cf7192fd1566becc662dbb3f5dec1e
MarwanEsm <marwan.esmaail85@.com>
2021-03-10 10:46:00 +0100

    Filter search

--- client/src/landingPageComponents/navbar.js
+++ client/src/landingPageComponents/navbar.js
@@ -1,16 +1,14 @@
-import React, { useState, useContext } from "react";
+import React, {useContext } from "react";
 import Logo from "./Logo";
 import Navbar from "react-bootstrap/Navbar";
-import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 import { CandidatesContext } from "../CandidatesContext/CandidatesContextProvider";
 
 function Headbar() {
   const {
-    searchLocation,
-    setSearchLocation,
     searchTitle,
     setSearchTitle,
     filteredCandidates,
@@ -22,42 +20,31 @@ function Headbar() {
         <Logo />
         <Form style={formStyle}>
           <div style={divStyle}>
-          <select>{filteredCandidates.length &&filteredCandidates.map((candidate)=>{
-            return (
-              <option>{candidate.location}</option>
-            )
-          })}
-            
-          </select>
-          {/* <FormControl
-            type="text"
-            name="where"
-            placeholder="Where"
-            className="mr-sm-2"
-            value={searchLocation}
-            onChange={(e) => setSearchLocation(e.target.name)}
-          /> */}
-          <FormControl
-            type="text"
-            name="what"
-            placeholder="What"
-            className="mr-sm-2"
-            value={searchTitle}
-            onChange={(e) => setSearchTitle(e.target.name)}
-          />
-          {/* <Button
-            variant="outline-primary" onClick ={(e)=> setSearchLocation(e.target.value)}
-          >
-            Search
-          </Button> */}
+            <select>
+              {filteredCandidates.length &&
+                filteredCandidates.map((candidate) => {
+                  return <option>{candidate.location}</option>;
+                })}
+            </select>
+
+            <FormControl
+              type="text"
+              name="what"
+              placeholder="What"
+              className="mr-sm-2"
+              value={searchTitle}
+              onChange={(e) => setSearchTitle(e.target.name)}
+            />
           </div>
         </Form>
-        <Nav className="mr-auto" style={navStyle}>
-          <Nav.Link href="/Landingpage">Candidates </Nav.Link>
-          <Nav.Link href="Registration">Register</Nav.Link>
-          <h5 style={h6Style}>|</h5>
-          <Nav.Link href="Loging">Login</Nav.Link>
-        </Nav>
+        <div style={div1Style}>
+          <Nav className="mr-auto" style={navStyle}>
+            <Link to="/Landingpage">Candidates </Link>
+            <Link to="/Registration">Register</Link>
+            <h5 style={h6Style}>|</h5>
+            <Link to="/Login">Login</Link>
+          </Nav>
+        </div>
       </Navbar>
       <br />
     </div>
@@ -79,10 +66,14 @@ const formStyle = {
   width: "80%",
 };
 
-const divStyle={
-  display:'flex',
-  justifyContent :'space-around',
- 
-}
+const divStyle = {
+  display: "flex",
+  justifyContent: "space-around",
+};
+
+const div1Style = {
+  display: "flex",
+  justifyContent: "space-around",
+};
 
 export default Headbar;
