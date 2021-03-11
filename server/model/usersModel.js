4a1d9795c4549309199548fba4bd01adeb8b8dd3
MarwanEsm <marwan.esmaail85@.com>
2021-03-11 13:59:31 +0100

    Adding validator to the registration

--- server/model/usersModel.js
+++ server/model/usersModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   firstName: { type: String },
   lastName: { type: String },
-  email: { type: String, required: true },
+  email: { type: String, validator : true, required: true },
   password: { type: String, required: true },
   city: { type: String, required: true },
   country: { type: String, required: true },
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,26 +1,47 @@
-const express = require ('express');
-const UserSchema = require('../model/usersModel');
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const UserSchema = require("../model/usersModel");
 const router = express.Router();
 
-
-router.get('/all', (req, res) =>{
-    UserSchema.find({}, (err, users) =>{
-    if(err){
-        res.send(err)
-    }else{
-        res.send(users)
+router.get("/all", (req, res) => {
+  UserSchema.find({}, (err, users) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(users);
     }
-})})
-
-
-router.post('/new', (req,res)=>{
-    const newUser = new UserSchema(req.body);
-    newUser.save().then((user) =>{
-        res.send(user)
-    }) .catch((err)=>{
-        res.send(err)
-    });
+  });
 });
-        
-       
+
+// router.post('/new', (req,res)=>{
+//     const newUser = new UserSchema(req.body);
+//     newUser.save().then((user) =>{
+//         res.send(user)
+//     }) .catch((err)=>{
+//         res.send(err)
+//     });
+// });
+
+router.post(
+  "/new",
+  body("email").isEmail(),
+  body("password").isLength({ min: 9 }),
+  (req, res) => {
+      const err = validationResult(req);
+      if(!err.isEmpty()){
+          res.send(err)
+      }else{
+    const newUser = new UserSchema(req.body);
+    newUser
+      .save()
+      .then((user) => {
+        res.send(user);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+    }
+  }
+);
+
 module.exports = router;
