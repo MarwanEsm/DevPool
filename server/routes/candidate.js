7b5a21a69bdf933172b0c842f58190d91733cb11
MarwanEsm <marwan.esmaail85@.com>
2021-03-25 16:20:01 +0100

    Extract email from token

--- server/routes/candidate.js
+++ server/routes/candidate.js
@@ -2,6 +2,7 @@ const express = require("express");
 const CandidateSchema = require("../model/candidatesModel");
 const router = express.Router();
 const multer = require("multer");
+const usersModel = require("../model/usersModel");
 
 const storage = multer.diskStorage({
   destination: "uploads/",
@@ -22,43 +23,56 @@ router.get("/all", (req, res) => {
   });
 });
 
-router.post("/new", upload.single("file"), (req, res) => {
-  //  jwt.encode(token, (err, payload)=>{
-  //   if (err) {
-  //     res.send(err)
-  //   }else{
-  //     const payload={
+router.post(
+  "/new",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("file"),
+  (req, res) => {
+    //  jwt.encode(token, (err, payload)=>{
+    //   if (err) {
+    //     res.send(err)
+    //   }else{
+    //     const payload={
 
-  //       id : user.id,
-  //       email : user.email
-  //     }
+    //       id : user.id,
+    //       email : user.email
+    //     }
 
-  //   }
-  // })
+    //   }
+    // })
 
-  const newEmail =
-    req.body
-      .email; /*compare the email from the encoded token with  entered email*/
-  CandidateSchema.findOne({ email: newEmail }, (err, candidate) => {
-    if (err) {
-      res.send(err);
-    }
-    if (candidate) {
-      res.send({ msg: "Candidate is already registered" });
-    } else {
-      const body = { ...req.body, img: `uploads/${req.file.originalname}` };
-      const newCandidate = new CandidateSchema(body);
-      newCandidate
-        .save()
-        .then((candidate) => {
-          console.log(candidate);
-          res.send(candidate);
-        })
-        .catch((err) => {
-          res.send(err);
-        });
-    }
-  });
-});
+    UserSchema.findOne({ email: user.email }),
+      (err, user) => {
+        if (err) {
+          res.send({ msg: "User does not exist" });
+        } else if (user) {
+          const newEmail =
+            user.email; /*compare the email from the encoded token with  entered email*/
 
+          CandidateSchema.findOne({ email: newEmail }, (err, candidate) => {
+            if (err) {
+              res.send(err);
+            } else if (candidate) {
+              res.send({ msg: "Candidate is already registered" });
+            } else {
+              const body = {
+                ...req.body,
+                img: `uploads/${req.file.originalname}`,
+              };
+              const newCandidate = new CandidateSchema(body);
+              newCandidate
+                .save()
+                .then((candidate) => {
+                  console.log(candidate);
+                  res.send(candidate);
+                })
+                .catch((err) => {
+                  res.send(err);
+                });
+            }
+          });
+        }
+      };
+  }
+);
 module.exports = router;
