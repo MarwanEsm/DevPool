8f90e3428fa61468a6610ce2cebc6edacf899a23
MarwanEsm <marwan.esmaail85@gmail.com>
2021-04-18 16:34:58 +0200

    Styling individual  Profile Page

--- client/src/Individualprofile/NavBarIndividualCandidate.js
+++ client/src/Individualprofile/NavBarIndividualCandidate.js
@@ -1,32 +1,42 @@
 import React, { useContext } from "react";
 import Logo from "../LandingPageComponents/Logo";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { AuthContext } from "../ContextProvider/AuthContextProvider";
-
+import Badge from "react-bootstrap/Badge";
 
 function NavBarEditProfileCan() {
   const { user } = useContext(AuthContext);
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push("/ChatScreen");
+  };
+
+  const handleClick1 = () => {
+    history.push(`/EditProfileCandidate/${user._id}`);
+  };
+
+  const handleClick2 = () => {
+    history.push("/");
+  };
+
   return (
     <div style={maindivStyle}>
-      <div style={logoStyle}>
+      <div>
         <Logo />
       </div>
       <div style={navdiv}>
-      <Link to='/ChatScreen' style={linkStyle}>
-          
+        <Badge style={badg} variant="primary" onClick={handleClick}>
           Messages
-        </Link>
+        </Badge>
         &nbsp; &nbsp;
-        <h5>|</h5>
-       <Link to={`/EditProfileCandidate/${user._id}`} style={linkStyle}>
+        <Badge style={badg} variant="primary" onClick={handleClick1}>
           Edit Profile
-        </Link>
+        </Badge>
         &nbsp; &nbsp;
-        <h5>|</h5>
-        &nbsp; &nbsp;
-        <Link to="/" style={linkStyle}>
-          Logout
-        </Link>
+        <Badge style={badg} variant="primary" onClick={handleClick2}>
+          Log out
+        </Badge>
       </div>
     </div>
   );
@@ -34,27 +44,25 @@ function NavBarEditProfileCan() {
 
 const maindivStyle = {
   display: "flex",
-  justifyContent: "space-around",
-  backgroundColor: "#F0FFF0",
+  justifyContent: "space-between",
   paddingTop: "1%",
   marginTop: "0%",
-  paddingBottom: "1%",
+  marginRight: "3%",
+  marginLeft: "1.3%",
 };
-
 const navdiv = {
   display: "flex",
   justifyContent: "space-between",
-
 };
 
-const logoStyle = {
-  marginRight: "54%",
+const badg = {
+  height: 30,
+  width: 120,
+  fontSize: 15,
+  cursor: "pointer",
+  borderRadius: 12,
+  padding: 7,
+  alignText: "center",
 };
-
-const linkStyle = {
-  fontFamily: "Zapf Chancery, cursive",
-};
-
-
 
 export default NavBarEditProfileCan;
