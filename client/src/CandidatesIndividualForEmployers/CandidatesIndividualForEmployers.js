4e7da3205b7f8ba343e20ca73e8330564163bf41
MarwanEsm <marwan.esmaail85@gmail.com>
2021-04-27 09:15:56 +0200

    Modifying socila media icons

--- client/src/CandidatesIndividualForEmployers/CandidatesIndividualForEmployers.js
+++ client/src/CandidatesIndividualForEmployers/CandidatesIndividualForEmployers.js
@@ -188,7 +188,7 @@ function CandidateIndividualForEmployer() {
                               strokeWidth="2"
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                              className="feather feather-globe mr-2 icon-inline"
+                              className="feather feather-github mr-2 icon-inline"
                             >
                               <circle cx="12" cy="12" r="10"></circle>
                               <line x1="2" y1="12" x2="22" y2="12"></line>
